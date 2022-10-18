14b0889f46a567d86a019ce7ec93a1d7235d9c85
opzerg <opzerg9378@gmail.com>
2022-10-19 00:24:27 +0900

    feature: added .gitignore file feature: added DefaultApp project

---
+++ DefaultApp/App.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace DefaultApp;
+
+public class App : BackgroundService
+{
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        Console.WriteLine("Hello World!!!");
+        return Task.CompletedTask;
+    }
+}
---
+++ DefaultApp/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+using DefaultApp;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+await Host.CreateDefaultBuilder()
+    .ConfigureHostConfiguration(configure =>
+    {
+        // 실행순서
+        Console.WriteLine("first");
+    })
+    .ConfigureAppConfiguration((context, configure) =>
+    {
+        // 실행 인자 값 --environment로 넘어온 값이  
+        // HostBuilderContext.HostingEnvironment.EnvironmentName에 set 된다
+        // 위를 활용하여 설정 파일을 Development 환경과 Production 환경을 분리하여 관리할 수 있다.(launchSettings.json 참고)
+        // 
+        Console.WriteLine($"second {context.HostingEnvironment.EnvironmentName}");
+    })
+    .ConfigureServices((context, services) =>
+    {
+        Console.WriteLine("third");
+        // 호스트앱 설정을 위해서는 IHostedService interface를 확장하거나
+        // IHostedService를 확장하고 있는 BackgroundService 내장 class를 상속받아도 된다.
+        services.AddHostedService<App>();
+    })
+    .RunConsoleAsync();
