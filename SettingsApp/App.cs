413c08a0fb5e82ec3e460ba91f784ca225cb2ef3
opzerg <opzerg9378@gmail.com>
2022-10-22 22:55:18 +0900

    feature: sln 파일 추가
    feature: 환경별로 appsettings load 하는 코드 작성
    
    update: .gitignore .idea 경로 추가

---
+++ SettingsApp/App.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace SettingsApp;
+
+internal class App : BackgroundService
+{
+    private readonly IConfiguration _configuration;
+
+    public App(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        Console.WriteLine("Run as App");
+        
+        // appsettings에 각각 설정되어 있는 값을 출력
+        Console.WriteLine($"{_configuration.GetValue<string>("Print")}");
+        return Task.CompletedTask;
+    }
+}
---
+++ SettingsApp/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SettingsApp;
+
+// 실행 상태가 개발 환경인 경우와 실 환경인 경우를 나눠서 설정 파일을
+// 관리해야 하는 경우
+
+// 개발환경 설정 파일: appsettings.Development.json
+// 실환경 설정 파일: appsettings.Production.json
+
+// 기본적으로 아무 세팅을 하지 않으면 
+// context.HostingEnvironment.EnvironmentName = Production
+
+await Host.CreateDefaultBuilder()
+    .ConfigureHostConfiguration(configure =>
+    {
+        // (필수) 실행 인자 값을 set
+        configure.AddCommandLine(args);
+    })
+    .ConfigureAppConfiguration((context, configure) =>
+    {
+        // (선택) default로 설정되어 있는 source 파일들을 clear 해줌
+        configure.Sources.Clear();
+        
+        // 설정 파일을 추가
+        // 추가할 때 실행한 환경이 개발환경인지 실환경인지에 따라 설정파일 load
+        configure.AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json");
+    })
+    .ConfigureServices((context, services) =>
+    {
+        services.AddHostedService<App>();
+    })
+    .RunConsoleAsync();
+
